52e419839abfb28a6b5d463dd7cfd66ecdc70039
= <=>
2022-08-06 10:26:55 +0100

    hook useField criado

--- src/components/AddTransactions.tsx
+++ src/components/AddTransactions.tsx
@@ -1,29 +1,17 @@
 import { Box, TextField, FormLabel } from "@mui/material";
-import { useState } from "react";
+import useField from "../hooks/useField";
 import { TextWithDivider } from "../styles/General";
 
 export default function AddTransactions() {
-  const [field, setField] = useState({
-    value: "",
-    error: false,
-    helperText: "",
-  });
-  function handleChange(
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) {
-    const { value } = e.target;
-    if (!value) {
-      setField({ value, error: true, helperText: "Campo inválido" });
-      return;
-    }
-    setField({ value, error: false, helperText: "" });
-  }
+  const [nameField, handleChangeName] = useField();
+  const [valueField, handleChangeValue] = useField();
+
   return (
     <Box mt={4}>
       <TextWithDivider>Adicionar transação</TextWithDivider>
       <Box mt={2}>
         <Box mb={0.7}>
-          <FormLabel error={field.error}>Nome</FormLabel>
+          <FormLabel error={nameField.error}>Nome</FormLabel>
         </Box>
         <TextField
           variant="outlined"
@@ -31,8 +19,22 @@ export default function AddTransactions() {
           size="small"
           placeholder="Nome da transação"
           fullWidth
-          {...field}
-          onChange={handleChange}
+          {...nameField}
+          onChange={handleChangeName}
+        />
+      </Box>
+      <Box mt={1}>
+        <Box mb={0.7}>
+          <FormLabel error={valueField.error}>Valor</FormLabel>
+        </Box>
+        <TextField
+          variant="outlined"
+          color="primary"
+          size="small"
+          placeholder="Valor da transação"
+          fullWidth
+          {...valueField}
+          onChange={handleChangeValue}
         />
       </Box>
     </Box>
---
+++ src/hooks/useField.ts
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+interface FieldProps {
+  value?: string;
+  error?: boolean;
+  helperText?: string;
+}
+
+type handleChangeFunc = (
+  e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | undefined>
+) => void;
+
+export default function useField(): [FieldProps, handleChangeFunc] {
+  const [field, setField] = useState<FieldProps>({
+    value: "",
+    error: false,
+    helperText: "",
+  });
+  const handleChange: handleChangeFunc = (e) => {
+    const value = e?.target.value;
+    if (!value) {
+      setField({ value: "", error: true, helperText: "Campo inválido" });
+      return;
+    }
+    setField({ value, error: false, helperText: "" });
+  };
+
+  return [field, handleChange];
+}
