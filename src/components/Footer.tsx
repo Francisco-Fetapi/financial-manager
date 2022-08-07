9597b63ba395d5c291581d85832fc562b3e47728
= <=>
2022-08-07 06:20:30 +0100

    Footer criado - onde estara o toggle button para alterar o tema

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import { Text } from "../styles/General";
+
+export default function Footer() {
+  return (
+    <Box mt={2}>
+      <Text color="textSecondary" align="center">
+        Copyrights - Francisco Fetapi
+      </Text>
+    </Box>
+  );
+}
